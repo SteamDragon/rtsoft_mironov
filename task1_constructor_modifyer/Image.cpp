e7a570c99cb08fca36d3b86e3c3118d1dc970aab
Anton Mironov <98mirant@gmail.com>
2018-02-19 17:12:23 +0300

    Added realisation for copyConstructor and operator=

---
+++ task1_constructor_modifyer/Image.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "Image.h"
+
+Image::Image(int tmy, int tmx):mx(tmx),my(tmy)
+{
+	img = new int[tmx*tmy];
+}
+
+Image::Image(const Image & image)
+{
+	img = image.img;
+	mx = image.mx;
+	my = image.my;
+}
+
+Image::~Image()
+{
+	delete []img;
+}
+
+void Image::load(char* fname)
+{
+}
+
+void Image::show()
+{
+	for (int i = 0; i < my; i++) {
+		for (int j = 0; j < mx; j++)
+		{
+			std::cout << get(i, j) << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void Image::init()
+{
+	for (int i = 0; i<my; i++)
+		for (int j = 0; j < mx; j++)
+		{
+			set(i,j,0);
+		}
+}
+
+void Image::set(int y, int x, int c)
+{
+	img[mx*y + x] = c;
+}
+int Image::get(int y, int x)
+{
+	return img[mx*y + x];
+}
+
+Image & Image::operator=(const Image & image)
+{
+	this->img = image.img;
+	this->mx = image.mx;
+	this->my = image.my;
+	return *this;
+}
